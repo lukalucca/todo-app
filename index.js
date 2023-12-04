4c65266a91bad017853d47558a55ec42f6bf2ce9
Lucca Alves <lucca.al@hotmail.com>
2023-12-04 04:25:14 -0300

    conexão com o mysql finalizada

--- index.js
+++ index.js
@@ -14,13 +14,21 @@ app.get('/', (requisicao, resposta) => {
 })
 
 const conexao = mysql.createConnection({
-    host: "localhost"
+    host: "localhost",
     user: "root",
     password: "root",
-    database: "todoapp"
+    database: "todoapp",
     port: 3306
 })
 
-app.listen(3000,() => {
-    console.log("Servidor rodando na porta 3000!")
+conexao.connect((erro) =>{
+    if (erro) {
+        return console.log(erro)
+    }
+
+    console.log("Estou conectado ao MySQL")
+
+    app.listen(3000,() => {
+        console.log("Servidor rodando na porta 3000!")
+    })
 })
